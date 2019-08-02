13bd0a95ce945cb826102d0ee38fe87d179cf1aa
Kim Yong Lim <kimyong@Kims-MacBook-Pro.local>
2019-08-02 23:34:51 +0800

    update menu-items to navigate withRouter HOC

--- src/components/directory/Directory.jsx
+++ src/components/directory/Directory.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import { sections } from "./directory.data";
 import MenuItem from "../menu-item/menu-item";
-import './directory.styles.scss';
+import "./directory.styles.scss";
 
 class Directory extends Component {
   constructor() {
@@ -15,8 +15,8 @@ class Directory extends Component {
     const { sections } = this.state;
     return (
       <div className="directory-menu">
-        {sections.map(({ id, title, imageUrl, size }) => {
-          return <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />;
+        {sections.map(({ id, ...otherSectionProps }) => {
+          return <MenuItem key={id} {...otherSectionProps} />;
         })}
       </div>
     );
--- src/components/menu-item/menu-item.jsx
+++ src/components/menu-item/menu-item.jsx
@@ -1,10 +1,18 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
+
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size}) => {
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
   return (
-    <div className={`${size} menu-item`}>
-      <div className='background-image' style={{backgroundImage: `url(${imageUrl})`} } />
+    <div
+      className={`${size} menu-item`}
+      onClick={() => history.push(`${match.url}${linkUrl}`)}
+    >
+      <div
+        className="background-image"
+        style={{ backgroundImage: `url(${imageUrl})` }}
+      />
       <div className="content">
         <h1 className="title">{title}</h1>
         <span className="subtitle">SHOP NOW</span>
@@ -13,4 +21,4 @@ const MenuItem = ({ title, imageUrl, size}) => {
   );
 };
 
-export default MenuItem;
+export default withRouter(MenuItem);
